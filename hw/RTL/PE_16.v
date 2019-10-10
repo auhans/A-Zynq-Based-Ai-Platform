a223b3b2ba126a497858b6e073d91dac054d30bc
auhans <auchhans@gmail.com>
2019-10-10 19:09:14 +0800

    Update PE_16.v
    refactored design of PE16bit

--- hw/RTL/PE_16.v
+++ hw/RTL/PE_16.v
@@ -1,53 +1,53 @@
 module PE_16 (
         // controls
 		input wire clk,
-		input wire en,
         input wire simd_mode,
-        // data input
-        input wire weight_write_en,
-		input wire [15:0] weight_data,
-		input wire [15:0] matrix_data,
-		input wire [ 1:0] matrix_sign,
+        input wire is_weight,
+        // data
+		input wire [15:0] data,
 		input wire [31:0] sum_in,
-        // data output
-		output reg [31:0] sum_out,
-		output reg [31:0] matrix_data_pass,
-		output reg [ 1:0] matrix_sign_pass,
+        // output
+        output reg is_weight_pass,
+		output reg [15:0] data_pass,
+		output reg [31:0] sum_out
 	);
 
     // declaration
-    reg [15:0] weight, matrix;
-    reg [31:0] sum, product;
+    reg [15:0] weight;
+    reg [31:0] sum;
 
-    // sequenctial update logic
-    always @(posedge clk) begin
-        // pass data update
-        matrix_data_pass <= matrix_data;
-        matrix_sign_pass <= matrix_sign;
-        sum_out <= sum;
-        // weight update
-        if ( weight_write_en ) begin
-            weight <= weight_data;
+    // sequenctial state logic
+    always @( posedge clk or negedge rst_n ) begin
+        if ( ~rst_n ) begin
+            // reset values
+            is_weight_pass <= 0;
+            data_pass <= 0;
+            sum_out <= 0;
+            weight <= 0;
         end
         else begin
-            weight <= weight;
+            // data update
+            is_weight_pass <= is_weight;
+            data_pass <= data;
+            sum_out <= sum;
+            // update selector
+            if ( is_weight ) begin
+                weight <= data;
+            end
+            else begin
+                weight <= weight;
+            end
         end
     end
 
-    // combintaional logics
+    // combintaional logics output
     always @(*) begin
         if ( simd_mode == 0 ) begin // 8bit mode
-            // multiplication
-            // ...
-            // addition
-            sum[ 7:0] <= sum_in[ 7:0] + product[ 7:0];
-            sum[15:8] <= sum_in[15:8] + product[15:8];
+            sum[ 7:0] = matrix_data[ 7:0] * weight[ 7:0] + sum_in[ 7:0];
+            sum[15:8] = matrix_data[15:8] * weight[15:8] + sum_in[15:8];
         end
         else if ( simd_mode == 1 ) begin // 16bit mode
-            // multiplication
-            // ...
-            // addition
-            sum <= sum_in + product;
+            sum = matrix_data * weight + sum_in;
         end
     end
 
