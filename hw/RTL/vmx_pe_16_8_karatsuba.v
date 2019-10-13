57a6feb0bbd5ab7f12992a25286f98f6ab4a2080
yaonepiece <yaonepiece@gmail.com>
2019-10-13 15:53:33 +0800

    Revise some code. Add a new version of karatsuba-designed PE.

--- hw/RTL/vmx_pe_16_8_karatsuba.v
+++ hw/RTL/vmx_pe_16_8_karatsuba.v
@@ -1,7 +1,7 @@
 
 `timescale 1 ns / 1 ps
 
-module vmx_pe_16_8 #(
+module vmx_pe_16_8_karatsuba #(
     parameter VECTOR_BITLEN = 16,
     parameter PRODCUT_BITLEN = VECTOR_BITLEN * 2
 )
@@ -24,6 +24,12 @@ module vmx_pe_16_8 #(
     // declaration
     reg [VECTOR_BITLEN-1:0] weight;
     reg [PRODCUT_BITLEN-1:0] sum;
+    
+    wire [15:0] product_low;
+    wire [15:0] product_high;
+    wire [15:0] product_mid;
+    wire [15:0] product_trim;
+    wire [31:0] product_full;
 
     // sequenctial state logic
     always @( posedge clk or negedge rst_n ) begin
@@ -50,15 +56,22 @@ module vmx_pe_16_8 #(
             end
         end
     end
-
+    
+    // karatsuba algorithm
+    assign product_low  = data[ 7:0] * weight[ 7:0];
+    assign product_high = data[15:8] * weight[15:8];
+    assign product_mid  = (data[15:8] + data[7:0]) * (weight[15:8] + weight[7:0]);
+    assign product_trim = product_mid - (product_high + product_low);
+    assign product_full = {product_high, product_low} + {product_trim, 8'b0};
+    
     // combinational logics output
     always @(*) begin
         if ( simd_mode == 1 ) begin // 8bit mode
-            sum[15: 8] = data[ 7:0] * weight[ 7:0] + sum_in[15: 0];
-            sum[31:16] = data[15:8] * weight[15:8] + sum_in[31:16];
+            sum[15: 0] = product_low  + sum_in[15:8];
+            sum[31:16] = product_high + sum_in[31:16];
         end
         else begin // 16bit mode
-            sum = data * weight + sum_in;
+            sum = product_full + sum_in;
         end
     end
 
