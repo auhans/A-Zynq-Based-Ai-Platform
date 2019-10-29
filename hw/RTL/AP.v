5a4b3424bb938ac4fac0260082c9007e0cd9c2ba
Eric Chen <koc4466677@gmail.com>
2019-10-29 11:50:49 +0800

    fix queue and processing bug

--- hw/RTL/AP.v
+++ hw/RTL/AP.v
@@ -24,7 +24,8 @@ module AP#(
     parameter ISA = 2,
               BITLEN = 64,
               ADDR = 32,
-              BUFFER_SIZE = 8)
+              BUFFER_SIZE = 8,
+              BUFFER_BIT = 3)
 (
     input clk,
     input rst_n,
@@ -42,7 +43,7 @@ module AP#(
     input [BITLEN-1:0] EAQ2_FIFO_dout,
    
     output ECN,
-    output reg DMA_burst,
+    output reg [7:0] DMA_burst,
     output DMA_FIFO1_rd_en,
     output ISA_FIFO_rd_en,
     output reg AEQ_FIFO_din,
@@ -67,12 +68,14 @@ reg[BUFFER_SIZE-1:0] DMA_tail = 0;
 reg[BUFFER_SIZE-1:0] EAQ_head = 0;
 reg[BUFFER_SIZE-1:0] EAQ_tail = 0;
 
-reg [ISA-1:0] processing_ISA;
-reg [ADDR-1:0] processing_mem;
-reg[1:0] curr_state = 0;
-reg[1:0] next_state = 0;
+reg [ISA-1:0] processing_ISA = 0;
+reg [ADDR-1:0] processing_mem = 0;
 reg EAQ_counter = 0;
 
+reg [1:0] HI = 0;
+reg [7:0] command_counter = 0;
+wire processing_flag;
+assign processing_flag = ~(processing_ISA==0);
 assign ECN = AEQ_FIFO_full;
 assign DMA_FIFO1_rd_en = ~DMA_FIFO1_empty;
 assign ISA_FIFO_rd_en = ~ISA_FIFO_empty;
@@ -81,51 +84,65 @@ assign EAQ2_FIFO_rd_en = ~EAQ2_FIFO_empty;
 assign AEQ_FIFO_wr_en = ~AEQ_FIFO_full;
 
 // handle received Instruction
+// 8 cases
 always @(posedge clk) begin
     if(rst_n) begin
         ISA_queue[0] <= 0;
-        mem_queue[0] <= 0;
-        mem_head <= 0;
-        ISA_head <= 0;
-    end
-    else if(~ISA_FIFO_empty) begin
-        ISA_queue[ISA_head] <= ISA_FIFO_dout;
-        mem_queue[mem_head] <= ISA_FIFO_dout;
-        mem_head = mem_head+1;
-        ISA_head = ISA_head+1;
-    end
-    else begin
-        mem_head <= mem_head;
-        ISA_head <= ISA_head;
-    end
-end
-
-// handle the ISA_queue
-always @(posedge clk) begin
-    if(rst_n) begin
-        ISA_tail <= 0;
         processing_ISA <= 0;
-    end
-    else if( (processing_ISA==0) && (ISA_tail!=ISA_head))begin
-        processing_ISA = ISA_queue[ISA_tail];
-        ISA_tail = ISA_tail+1;
-    end
-    else begin
-        ISA_tail <= ISA_tail;
-    end
-end
-
-// handle the mem_queue
-always @(posedge clk) begin
-    if(rst_n) begin
+        ISA_head <= 0;
+        ISA_tail <= 0;
+        mem_queue[0] <= 0;
+        processing_mem <= 0;
+        mem_head <= 0;
         mem_tail <= 0;
     end
-    else if( (processing_ISA==0) && (mem_tail!=mem_head))begin
-        processing_mem = mem_queue[mem_tail];
-        mem_tail = mem_tail+1;
+    else if( (ISA_FIFO_empty) && (processing_flag) && (ISA_tail==ISA_head) ) begin
+        ISA_head <= ISA_head;
+        mem_head <= mem_head;
     end
-    else begin
-        mem_tail <= mem_tail;
+    else if( (ISA_FIFO_empty) && (processing_flag) && (ISA_tail!=ISA_head) ) begin
+        ISA_head <= ISA_head;
+        mem_head <= mem_head;
+    end
+    else if( (ISA_FIFO_empty) && (~processing_flag) && (ISA_tail==ISA_head) ) begin
+        ISA_head <= ISA_head;
+        mem_head <= mem_head;
+    end
+    else if( (ISA_FIFO_empty) && (~processing_flag) && (ISA_tail!=ISA_head) ) begin
+        processing_ISA <= ISA_queue[ISA_tail];
+        processing_mem <= mem_queue[mem_tail];
+        ISA_tail <= ISA_tail+1;
+        mem_tail <= mem_tail+1;
+    end
+    else if( (~ISA_FIFO_empty) && (processing_flag) && (ISA_tail==ISA_head) ) begin
+        ISA_queue[ISA_head] <= ISA_FIFO_dout[33:32]; 
+        mem_queue[mem_head] <= ISA_FIFO_dout[31:0];
+        ISA_head <= ISA_head+1;
+        mem_head <= mem_head+1;
+    end
+    else if( (~ISA_FIFO_empty) && (processing_flag) && (ISA_tail!=ISA_head) ) begin
+        ISA_queue[ISA_head] <= ISA_FIFO_dout[33:32]; 
+        mem_queue[mem_head] <= ISA_FIFO_dout[31:0];
+        ISA_head <= ISA_head+1;
+        mem_head <= mem_head+1;
+    end
+    else if( (~ISA_FIFO_empty) && (~processing_flag) && (ISA_tail==ISA_head) ) begin
+        processing_ISA <= ISA_FIFO_dout[33:32];
+        processing_mem <= ISA_FIFO_dout[31:0];
+        ISA_head <= ISA_head+1;
+        mem_head <= mem_head+1;
+        ISA_tail <= ISA_tail+1;
+        mem_tail <= mem_tail+1;
+    end
+    else if( (~ISA_FIFO_empty) && (~processing_flag) && (ISA_tail!=ISA_head) ) begin
+        ISA_queue[ISA_head] <= ISA_FIFO_dout[33:32]; 
+        mem_queue[mem_head] <= ISA_FIFO_dout[31:0];
+        ISA_head <= ISA_head+1;
+        mem_head <= mem_head+1;
+        processing_ISA <= ISA_queue[ISA_tail];
+        processing_mem <= mem_queue[mem_tail];
+        ISA_tail <= ISA_tail+1;
+        mem_tail <= mem_tail+1;
     end
 end
 
@@ -203,7 +220,7 @@ always @(posedge clk) begin
     if(rst_n) begin
         processing_ISA <= 0;
     end
-    else if(processing_ISA) begin
+    else if(processing_flag) begin
         case (processing_ISA)
             // read (32bit/per)
             2'b01: begin
@@ -215,9 +232,6 @@ always @(posedge clk) begin
             end
         endcase
         processing_ISA = 0;
-    end
-    else begin
-        processing_ISA <= processing_ISA;
     end
 end
 
